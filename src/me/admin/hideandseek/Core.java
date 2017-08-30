b91db189b8bd8551c65f3ea2c712d6b0051418f5
MintyCodes <mintercraft@outlook.com>
2017-08-30 10:16:58 +0100

    Commit
    30/08/2017

---
+++ src/me/admin/hideandseek/Core.java
@@ -0,0 +1,16 @@
+package me.admin.hideandseek;
+
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Core extends JavaPlugin implements Listener{
+	
+	public void onEnable() {
+		
+	}
+	
+	public void onDisbale() {
+		
+	}
+
+}

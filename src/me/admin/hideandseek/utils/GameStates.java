00fbe174ee551e818603eb65c144adb6eb21f635
MintyCodes <mintercraft@outlook.com>
2017-08-30 13:51:33 +0100

    Added GameStates

---
+++ src/me/admin/hideandseek/utils/GameStates.java
@@ -0,0 +1,30 @@
+package me.admin.hideandseek.utils;
+
+public enum GameStates {
+
+	IN_GAME ("In Game", false), PRE_GAME ("Pre Game", true), POST_GAME ("Post Game", false), REBOOTING ("Rebooting", false);
+	
+	
+	String gameState;
+	boolean canJoin;
+	
+	private GameStates(String state, boolean canJoin) {
+		gameState = state;
+		this.canJoin = canJoin;
+	}
+	
+	
+	public String toString() {
+		return gameState;
+	}
+	
+	public boolean isJoinable() {
+		return canJoin;
+	}
+	
+	public static GameStates[] getStates() {
+		GameStates[] gs = {IN_GAME, PRE_GAME, POST_GAME, REBOOTING};
+	
+		return gs;
+	}
+}

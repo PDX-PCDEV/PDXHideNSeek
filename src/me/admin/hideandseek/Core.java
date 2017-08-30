86ecb5e2f716c540360a3a7b7e9bc76616ad1237
GenuineAlex <alexgalaxydev@gmail.com>
2017-08-30 12:23:45 +0100

    Just a test

--- src/me/admin/hideandseek/Core.java
+++ src/me/admin/hideandseek/Core.java
@@ -1,6 +1,7 @@
 package me.admin.hideandseek;
 
 import org.bukkit.event.Listener;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Core extends JavaPlugin implements Listener{
@@ -8,13 +9,13 @@ public class Core extends JavaPlugin implements Listener{
 	public String PDXTAG = "[PDX-MC] ";
 	
 	public void onEnable() {
-		getLogger().info(PDX-TAG + "The Hide and Seek Gamemode has been activated!")
+		getLogger().info(PDXTAG + "The Hide and Seek Gamemode has been activated!")
 		PluginManager pm = getServer().getPluginManager();
-		pm.registerEvent(this, this);
+		pm.registerEvents(this, this);
 	}
 	
 	public void onDisbale() {
-		getLogger().info(PDX-TAG + "The Hide and Seek Gamemode has been deactivated!")
+		getLogger().info(PDXTAG + "The Hide and Seek Gamemode has been deactivated!")
 	}
 
 }

d6cf9b0a71520c369b0652866c1663d6cb193d68
GenuineAlex <alexgalaxydev@gmail.com>
2017-08-30 10:45:06 +0100

    Started to do work in the Core file

--- src/me/admin/hideandseek/Core.java
+++ src/me/admin/hideandseek/Core.java
@@ -5,13 +5,16 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class Core extends JavaPlugin implements Listener{
 	
+	public String PDXTAG = "[PDX-MC] ";
+	
 	public void onEnable() {
+		getLogger().info(PDX-TAG + "The Hide and Seek Gamemode has been activated!")
 		PluginManager pm = getServer().getPluginManager();
 		pm.registerEvent(this, this);
 	}
 	
 	public void onDisbale() {
-		
+		getLogger().info(PDX-TAG + "The Hide and Seek Gamemode has been deactivated!")
 	}
 
 }

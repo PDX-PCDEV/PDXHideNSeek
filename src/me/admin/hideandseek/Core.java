2e39c8fc92effdd40de1a9507b7c67970fb10aad
MintyCodes <mintercraft@outlook.com>
2017-08-30 14:04:30 +0100

    Removed seperate class for GameStates and set it up as Enum class
    Fixed Core class

--- src/me/admin/hideandseek/Core.java
+++ src/me/admin/hideandseek/Core.java
@@ -6,16 +6,14 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class Core extends JavaPlugin implements Listener{
 	
-	public String PDXTAG = "[PDX-MC] ";
 	
 	public void onEnable() {
-		getLogger().info(PDXTAG + "The Hide and Seek Gamemode has been activated!")
+		
 		PluginManager pm = getServer().getPluginManager();
 		pm.registerEvents(this, this);
 	}
 	
 	public void onDisbale() {
-		getLogger().info(PDXTAG + "The Hide and Seek Gamemode has been deactivated!")
 	}
 
 }
